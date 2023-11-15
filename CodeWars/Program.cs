6c4f776ece9a3f48a3ab2213dd34349aafaaf85f
spreedated <21370839+spreedated@users.noreply.github.com>
2023-11-15 04:13:57 +0100

    added cw main project

---
+++ CodeWars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Solutions
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
